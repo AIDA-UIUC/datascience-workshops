ae172682b5c7234f9777b7e276a8aa514b3c950a
Michael Xiang <mx9@illinois.edu>
2020-10-17 09:25:26 -0500

    stashing

--- wk08_dash/app.py
+++ wk08_dash/app.py
@@ -1,20 +1,43 @@
-import flask
 import dash
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
 import dash_html_components as html
 
-server = flask.Flask(__name__)
+from pandas_datareader import data as web
+from datetime import datetime as dt
 
-@server.route('/')
-def index():
-    return 'Hello Flask app'
+app = dash.Dash('Hello World')
 
-app = dash.Dash(
-    __name__,
-    server=server,
-    routes_pathname_prefix='/dash/'
-)
+app.layout = html.Div([
+    dcc.Dropdown(
+        id='my-dropdown',
+        options=[
+            {'label': 'Coke', 'value': 'COKE'},
+            {'label': 'Tesla', 'value': 'TSLA'},
+            {'label': 'Apple', 'value': 'AAPL'}
+        ],
+        value='COKE'
+    ),
+    dcc.Graph(id='my-graph')
+], style={'width': '500'})
 
-app.layout = html.Div("My Dash app")
+@app.callback(Output('my-graph', 'figure'), [Input('my-dropdown', 'value')])
+def update_graph(selected_dropdown_value):
+    df = web.DataReader(
+        selected_dropdown_value,
+        'google',
+        dt(2017, 1, 1),
+        dt.now()
+    )
+    return {
+        'data': [{
+            'x': df.index,
+            'y': df.Close
+        }],
+        'layout': {'margin': {'l': 40, 'r': 0, 't': 20, 'b': 30}}
+    }
+
+app.css.append_css({'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'})
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server()
