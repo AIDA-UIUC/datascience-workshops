a5a309ae16c36b18af66e00aa7b1b8ca5d10acca
Michael Xiang <47134569+mx101@users.noreply.github.com>
2020-10-17 12:50:11 -0500

    Delete wsgi.py

--- wk08_dash/wsgi.py
+++
@@ -1,9 +0,0 @@
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
-
-from flask_app import flask_app
-from dash_app import app
-
-
-application = DispatcherMiddleware(flask_app, {
-    '/dash': app.server,
-})
